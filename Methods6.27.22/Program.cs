52c0e0f561127823ee5aba70963a15381456788f
Taj <tajmarie92@gmail.com>
2022-06-27 21:26:05 -0500

    Add project files.

---
+++ Methods6.27.22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Methods6._27._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Add(1, 8);
+                if (result > 10)
+            {
+                Console.WriteLine("Result is larger than 10!");
+            }
+
+            else
+            {
+             Console.WriteLine("Result is smaller than or equal to 10!");
+            }
+                
+            Console.ReadKey();
+        }
+
+        public static int Add (int num01, int num02)
+        {
+            return num01 + num02;
+
+        }
+
+        
+    }
+}
